e9321a8092dce1e1d0d09d7713424c4b5550e4a4
waltertaya <waltermittybecker@gmail.com>
2024-05-24 19:19:33 +0300

    Added: allowed hosts

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movies-rest-api-dbp5.onrender.com']
 
 
 # Application definition
